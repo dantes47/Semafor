c4cb5621b0d6f7ce79da23fd8ceef5961674938c
Danny <dantes817@gmail.com>
2020-07-10 00:25:48 +0200

    Was finished graphic part

--- app/src/main/java/com/elmob/semafor/MainActivity.kt
+++ app/src/main/java/com/elmob/semafor/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.elmob.semafor
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.app.Activity
+import android.view.View
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : Activity() {
+    private var imgSemafor: ImageView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imgSemafor = findViewById(R.id.imgSemafor)
+    }
+
+    fun onClickRunStop(view: View) {
+        imgSemafor?.setImageResource(R.drawable.semafor_green)
     }
 }
